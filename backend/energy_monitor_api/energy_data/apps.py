b0f58f1a70d2179ba60b85c66babd10d020cf0a5
bernardolobomarques <bernardolobomarques@gmail.com>
2025-10-02 17:14:56 -0300

    Remove deprecated files and configurations for EnergyMonitor API

--- backend/energy_monitor_api/energy_data/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class EnergyDataConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'energy_data'
-    verbose_name = 'Dados de Energia'
